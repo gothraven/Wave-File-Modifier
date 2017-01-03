7dceb84eb0b96e7217aa213ddb03577d37f7444b
GothRaven <szaghbane@gmail.com>
2017-01-03 02:08:07 +0100

    02:08

--- args/args.c
+++ args/args.c
@@ -1,5 +1,3 @@
-
-
 #include "args.h"
 
 #include <stdlib.h>
@@ -12,15 +10,6 @@ option_t* opt_void(option_t* l, const char* kw, void (*f)()){
 	li->keyword = kw;
 	li->spec = OptVoid;
 	li->fct.opt_void = f;
-	li->next = l;
-	return li;
-}
-
-option_t* opt_unsignedInt(option_t* l, const char* kw, void (*f)(unsigned int)){
-	list li = (list)malloc(sizeof(option_t));
-	li->keyword = kw;
-	li->spec = OptUnsignedInt;
-	li->fct.opt_unsigned_int = f;
 	li->next = l;
 	return li;
 }
@@ -43,11 +32,11 @@ option_t* opt_2unsignedInt(option_t* l, const char* kw, void (*f)(unsigned int,u
 	return li;
 }
 
-option_t* opt_4unsignedInt(option_t* l, const char* kw, void (*f)(unsigned int,unsigned int,unsigned int,unsigned int)){
+option_t* opt_3unsignedInt(option_t* l, const char* kw, void (*f)(unsigned int,unsigned int,unsigned int)){
 	list li = (list)malloc(sizeof(option_t));
 	li->keyword = kw;
-	li->spec = Opt4UnsignedInt;
-	li->fct.opt_4unsigned_int = f;
+	li->spec = Opt3UnsignedInt;
+	li->fct.opt_3unsigned_int = f;
 	li->next = l;
 	return li;
 }
@@ -56,18 +45,16 @@ option_t* opt_4unsignedInt(option_t* l, const char* kw, void (*f)(unsigned int,u
 
 void process_arguments(option_t* l, int argc, char* *argv){
 	int i;
-	unsigned int nb1, nb2, nb3, nb4;
+	unsigned int nb1, nb2, nb3;
 	list cursor;
 	printf("\n");
-	for(i = 1; i < argc ; i+=2){	
+	for(i = 1; i < argc ; i+=2){
 		cursor = l;
 		while(cursor != NULL){
 			if(strcmp(cursor->keyword, argv[i]) == 0){
 				switch (cursor->spec){
 					case OptVoid: cursor->fct.opt_void();
-								  i--; 
-					break;
-					case OptUnsignedInt: cursor->fct.opt_unsigned_int((unsigned int) atoi(argv[i+i])); 
+								  i--;
 					break;
 					case OptString: cursor->fct.opt_str(argv[i+1]);
 					break;
@@ -76,16 +63,15 @@ void process_arguments(option_t* l, int argc, char* *argv){
 										  cursor->fct.opt_2unsigned_int(nb1,nb2);
 										  i++;
 					break;
-					case Opt4UnsignedInt: nb1 = (unsigned int) atoi(argv[i+1]);
+					case Opt3UnsignedInt: nb1 = (unsigned int) atoi(argv[i+1]);
 										  nb2 = (unsigned int) atoi(argv[i+2]);
 										  nb3 = (unsigned int) atoi(argv[i+3]);
-										  nb4 = (unsigned int) atoi(argv[i+4]);
-										  cursor->fct.opt_4unsigned_int(nb1,nb2,nb3,nb4);
-										  i+=3;
+										  cursor->fct.opt_3unsigned_int(nb1,nb2,nb3);
+										  i+=2;
 					break;
 					default: printf("Arguments not found");
 							 exit(EXIT_FAILURE);
-				}		
+				}
 			}
 			cursor = cursor->next;
 		}
